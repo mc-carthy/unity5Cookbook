133c6ac001055cca23a302acedda8710f5fe8b91
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-12 23:05:10 +0100

    01_07_MoveSceneButton

---
+++ 01_UI/07_MoveSceneButton/Assets/Scripts/MenuActions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuActions : MonoBehaviour {
+	public void MENU_ACTION_GotoPage(string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+}
