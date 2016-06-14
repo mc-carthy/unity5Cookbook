03fea6ab7f5b7d8f8ee4203328dff4f10166d3e3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-14 22:01:07 +0100

    02_08_PickupsDict

---
+++ 02_InventoryGUI/08_PickupsDict/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryManager : MonoBehaviour {
+	private PlayerInventoryDisplay playerInventoryDisplay;
+	private Dictionary<PickUp.PickUpType, int> items = new Dictionary<PickUp.PickUpType, int>();
+
+	void Start(){
+		playerInventoryDisplay = GetComponent<PlayerInventoryDisplay>();
+		playerInventoryDisplay.OnChangeInventory(items);
+	}
+
+	public void Add(PickUp pickup){
+		PickUp.PickUpType type = pickup.type;
+		int oldTotal = 0;
+		if(items.TryGetValue(type, out oldTotal))
+			items[type] = oldTotal + 1;
+		else
+			items.Add (type, 1);
+
+		playerInventoryDisplay.OnChangeInventory(items);
+	}
+}
