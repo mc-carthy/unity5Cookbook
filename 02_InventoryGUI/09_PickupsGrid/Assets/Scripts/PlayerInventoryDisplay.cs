34d9f87142abc995441c451ebbecc3c134746424
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-15 20:42:31 +0100

    02_09_PickupsGrid

--- 02_InventoryGUI/09_PickupsGrid/Assets/Scripts/PlayerInventoryDisplay.cs
+++ 02_InventoryGUI/09_PickupsGrid/Assets/Scripts/PlayerInventoryDisplay.cs
@@ -4,17 +4,38 @@ using UnityEngine.UI;
 
 public class PlayerInventoryDisplay : MonoBehaviour
 {
-	const int NUM_INVENTORY_SLOTS = 10;
-	public PickupUI[] slots = new PickupUI[NUM_INVENTORY_SLOTS];
+	const int NUM_INVENTORY_SLOTS = 15;
+	private PickupUI[] slots = new PickupUI[NUM_INVENTORY_SLOTS];
+	public GameObject slotGrid;
+	public GameObject starSlotPrefab;
+
+	void Awake(){
+		for(int i=0; i < NUM_INVENTORY_SLOTS; i++){
+			GameObject starSlotGO = (GameObject)
+				Instantiate(starSlotPrefab);
+			starSlotGO.transform.SetParent(slotGrid.transform);
+			starSlotGO.transform.localScale = new Vector3(1,1,1);
+			slots[i] = starSlotGO.GetComponent<PickupUI>();
+		}
+	}
+
+	void Start(){
+		float panelWidth = slotGrid.GetComponent<RectTransform>().rect.width;
+		print ("slotGrid.GetComponent<RectTransform>().rect = " + slotGrid.GetComponent<RectTransform>().rect);
+
+		GridLayoutGroup gridLayoutGroup = slotGrid.GetComponent<GridLayoutGroup>();
+		float xCellSize = panelWidth / NUM_INVENTORY_SLOTS;
+		xCellSize -= gridLayoutGroup.spacing.x;
+		gridLayoutGroup.cellSize = new Vector2(xCellSize, xCellSize);
+	}
 
 	public void OnChangeStarTotal(int starTotal){
 		for(int i = 0; i < NUM_INVENTORY_SLOTS; i++){
 			PickupUI slot = slots[i];
-			if (i < starTotal) {
-				slot.DisplayYellow ();
-			} else {
-				slot.DisplayGrey ();
-			}
+			if(i < starTotal)
+				slot.DisplayYellow();
+			else
+				slot.DisplayGrey();
 		}
 	}
 }
