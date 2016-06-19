679a69d8caa19727859862d5436ae09d971cce23
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-19 10:02:56 +0100

    03_01_HorizontalFlippingSprite

---
+++ 03_2DAnimation/01_HorizontalFlippingSprite/Assets/Scripts/BugFlip.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugFlip : MonoBehaviour {
+
+	public bool facingRight = true;
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && facingRight) {
+			Flip ();
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow) && !facingRight) {
+			Flip ();
+		}
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
