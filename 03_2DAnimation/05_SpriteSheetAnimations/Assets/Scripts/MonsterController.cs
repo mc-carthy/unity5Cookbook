e809d28d5118ceb0fe5dacbb90c7a577cd8c3b33
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-19 11:53:24 +0100

    03_05_SpriteSheetAnimations

---
+++ 03_2DAnimation/05_SpriteSheetAnimations/Assets/Scripts/MonsterController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterController : MonoBehaviour {
+
+	private Animator animatorController;
+
+	// Use this for initialization
+	void Start () {
+		animatorController = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			animatorController.SetTrigger ("Attack");
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			animatorController.SetTrigger ("Die");
+		}
+	}
+}
